1d44cd805e0760b437c11b8f5814441fe78285f4
Myles-Cork <38143953+Myles-Cork@users.noreply.github.com>
2023-03-23 11:57:06 -0600

    Add client api and db

---
+++ app/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
+from pymongo import MongoClient
+
+app = Flask(__name__)
+api = Api(app)
+
+client = MongoClient("mongodb://db:27017")
+db = client.ImageDB
+
+
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0')
